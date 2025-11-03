78ec5c88a31a89ed40dfad7063832f2294d1fac1
MrKubis <jakub.f102@gmail.com>
2025-11-03 10:52:00 +0100

    Init home controller

---
+++ backend/src/main/java/com/example/backend/api/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.backend.api.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+
+    @GetMapping
+    public String home(Principal principal){
+        return "Hello " + principal.getName();
+    }
+}
