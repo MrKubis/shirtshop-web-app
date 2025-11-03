2f0c818e6cb2f20e0b1c5b057d23661944166666
MrKubis <jakub.f102@gmail.com>
2025-11-03 14:12:25 +0100

    init: Cart Table

---
+++ backend/src/main/java/com/example/backend/api/cart/Cart.java
@@ -0,0 +1,81 @@
+package com.example.backend.api.cart;
+
+import com.example.backend.api.itemInstance.ItemInstance;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @ElementCollection
+    @CollectionTable(name="cart_items",joinColumns = @JoinColumn(name = "cart_id"))
+    @Column(name="list_item_instance_id")
+    private List<UUID> itemInstanceIdList;
+    @Column(name = "user_id")
+    private UUID user_id;
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+    @Column(name = "expired_at")
+    private  LocalDateTime expired_at;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public List<UUID> getItemInstanceIdList() {
+        return itemInstanceIdList;
+    }
+
+    public void setItemInstanceIdList(List<UUID> itemInstanceIdList) {
+        this.itemInstanceIdList = itemInstanceIdList;
+    }
+
+    public UUID getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(UUID user_id) {
+        this.user_id = user_id;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getExpired_at() {
+        return expired_at;
+    }
+
+    public void setExpired_at(LocalDateTime expired_at) {
+        this.expired_at = expired_at;
+    }
+
+    @PrePersist
+    public void onCreate(){
+        this.created_at = LocalDateTime.now();
+        this.expired_at = this.created_at.plusHours(1);
+    }
+    public Cart(){}
+    public Cart(List<UUID> itemInstanceIdList, UUID user_id) {
+        this.itemInstanceIdList = itemInstanceIdList;
+        this.user_id = user_id;
+        this.expired_at = getCreated_at().plusHours(1);
+    }
+}
---
+++ backend/src/main/java/com/example/backend/api/cart/CartController.java
@@ -0,0 +1,59 @@
+package com.example.backend.api.cart;
+
+import com.example.backend.api.itemInstance.ItemInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/carts")
+public class CartController {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Cart>> getCarts(){
+        if(cartRepository.findAll().isEmpty())
+        {
+            return ResponseEntity.notFound().build();
+        }
+        else return new ResponseEntity<List<Cart>>(cartRepository.findAll(), HttpStatus.OK);
+    }
+    @GetMapping(params = "user_id")
+    public ResponseEntity<List<Cart>> getCartsByUserId(@RequestParam UUID user_id){
+        Cart cart = new Cart();
+        cart.setUser_id(user_id);
+        Example<Cart> example = Example.of(cart);
+        Optional<List<Cart>> optional = Optional.of(cartRepository.findAll(example));
+        if(optional.isEmpty())
+            return ResponseEntity.notFound().build();
+        else return new ResponseEntity<List<Cart>>(cartRepository.findAll(example),HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Cart> createCart(@RequestBody Cart cart){
+        return new ResponseEntity<Cart>(cartRepository.save(cart),HttpStatus.CREATED);
+    }
+    @PutMapping
+    public ResponseEntity<Cart> editItem(@RequestBody Cart cart){
+        Optional<Cart> optional = cartRepository.findById(cart.getId());
+        if(optional.isPresent()){
+            Cart newcart = optional.get();
+            newcart.setUser_id(cart.getUser_id());
+            newcart.setCreated_at(cart.getCreated_at());
+            newcart.setExpired_at(cart.getCreated_at().plusHours(1));
+            newcart.setItemInstanceIdList(cart.getItemInstanceIdList());
+            cartRepository.save(newcart);
+            return new ResponseEntity<Cart>(newcart, HttpStatus.OK);
+        }
+        else return ResponseEntity.notFound().build();
+    }
+    //TODO ADD TO CART
+}
