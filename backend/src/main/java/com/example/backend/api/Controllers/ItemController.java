54151140b0ba353d76940a16a5d471f59f191562
MrKubis <jakub.f102@gmail.com>
2025-11-17 09:08:09 +0100

    feat: PreAuthorizations for ItemController

--- backend/src/main/java/com/example/backend/api/Controllers/ItemController.java
+++ backend/src/main/java/com/example/backend/api/Controllers/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +19,6 @@ public class ItemController {
 
     @Autowired
     private ItemRepository itemRepository;
-
     @GetMapping
     public ResponseEntity<List<Item>> getItems(){
         if(itemRepository.findAll().isEmpty()){
@@ -28,7 +28,6 @@ public class ItemController {
             return new ResponseEntity<List<Item>>(itemRepository.findAll(), HttpStatus.OK);
         }
     }
-
     @GetMapping(params = "type")
     public ResponseEntity<List<Item>> getItemsByType(@RequestParam String type){
         Item item = new Item();
@@ -39,7 +38,6 @@ public class ItemController {
             return ResponseEntity.notFound().build();
         else return  new ResponseEntity<List<Item>>(itemRepository.findAll(example),HttpStatus.OK);
     }
-
     @GetMapping(params = "id")
     public ResponseEntity<Item> getItemById(@RequestParam UUID id)
     {
@@ -47,10 +45,12 @@ public class ItemController {
         if(optional.isPresent()) return new ResponseEntity<Item>(optional.get(),HttpStatus.OK);
         else return ResponseEntity.notFound().build();
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<Item> createItem(@RequestBody Item item){
         return new ResponseEntity<Item>(itemRepository.save(item),HttpStatus.CREATED);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping
     public ResponseEntity<Item> editItem(@RequestBody Item item){
         Optional<Item> optional = itemRepository.findById(item.getId());
