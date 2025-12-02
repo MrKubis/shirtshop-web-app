496276159dd2ab7c6ce70275b56229ac2d8defc2
MrKubis <jakub.f102@gmail.com>
2025-12-02 19:55:57 +0100

    Init: Docker compose file with application.properties

--- backend/src/main/java/com/example/backend/api/Repositories/AuthorityRepository.java
+++ backend/src/main/java/com/example/backend/api/Repositories/AuthorityRepository.java
@@ -2,10 +2,12 @@ package com.example.backend.api.Repositories;
 
 import com.example.backend.api.Models.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, UUID> {
     Optional<Authority> findByName (String name);
 }
--- backend/src/main/java/com/example/backend/api/Repositories/RoleRepository.java
+++ backend/src/main/java/com/example/backend/api/Repositories/RoleRepository.java
@@ -2,10 +2,12 @@ package com.example.backend.api.Repositories;
 
 import com.example.backend.api.Models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     Optional<Role> findByName(String name);
 }
