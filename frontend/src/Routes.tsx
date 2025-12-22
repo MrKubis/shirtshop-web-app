385b4bb3fc3dd98fab3164c3d605dcf99f84a167
Jakub Frączek <jakub.f102@gmail.com>
2025-12-22 14:24:42 +0100

    init frontend project

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,12 @@
+import { RouteObject } from "react-router";
+import App from "./App";
+
+export const routes: RouteObject[] =[
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {}
+        ]
+    }
+]
